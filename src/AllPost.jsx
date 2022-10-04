870c5980ba08840dc61236d524eebf281ff74be9
Manu2015 <devries.webdesigner@gmail.com>
2022-10-04 23:59:03 +0200

    **fix** optimize AllPost fetch from two render to one

--- src/AllPost.jsx
+++ src/AllPost.jsx
@@ -4,27 +4,38 @@ const AllPost = ({ posts }) => {
   const [postsArray, setPostsArray] = useState(null)
 
   useEffect(() => {
-    const fetchPost = async () => {
-      const allPosts = posts.map(post => {
-        return fetch(`https://jsonplaceholder.typicode.com/${post}`).then((response) => response.json())
-      })
-      const all = await Promise.all(allPosts)
-      setPostsArray(all)
-    }
-    fetchPost()
-  }, [])
+    let isApiSubscribed = true;
+    isApiSubscribed &&
+      (async () => {
+        const allPosts = posts.map(post => {
+          return fetch(`https://jsonplaceholder.typicode.com/${post}`).then((response) => response.json())
+        })
+        Promise.all(allPosts)
+          .then((all) => { setPostsArray(all) })
+      })()
+    console.log('useefct');
+    return () => {
+      // cancel the subscription
+      isApiSubscribed = false;
+    };
+  }, [posts])
 
-  useEffect(() => {
+  /* useEffect(() => {
     console.log('postsArray', postsArray);
-  }, [postsArray])
+  }, [postsArray]) */
 
 
   return (
     <div>
       <h1>Hello AllPost : </h1>
       <ul>
-        {posts.map((i, x) => (
-          <li key={x}>{i}</li>
+        {postsArray && postsArray.map(({ userId, id, title, body }) => (
+          <li key={id}>
+            <h6>{title} </h6>
+            <p>
+              {body}
+            </p>
+          </li>
         ))}
       </ul>
 
